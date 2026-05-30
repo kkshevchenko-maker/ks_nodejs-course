b50b7a21847edddec2a1467cb17d02f50b461420
kkshevchenko-maker <k.k.shevchenko@student.khai.edu>
2026-05-31 00:27:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
-const express = require('express');
-const userRouter = require('./user/user.router');
-const productRouter = require('./product/product.router');
-const { logRequest } = require('./middleware');
-const bodyParser = require('body-parser');
-const { errorResponder } = require('./error.middleware');
+import { drizzle } from 'drizzle-orm/node-postgres';
 
-const app = express();
-const PORT = 3000;
+import { Client } from 'pg';
 
-app.use(bodyParser.json());
-app.use(logRequest);
-app.use(productRouter);
-app.use(userRouter);
-app.use(errorResponder);
+import * as schema from './schema';
 
-app.listen(PORT, () => {
-   console.log(`Server listening at http://localhost:${PORT}`);
+
+const client = new Client({
+
+   host: '127.0.0.1',
+
+   port: 5432,
+
+   user: 'nodejs_course_admin',
+
+   password: 'my_password',
+
+   database: 'nodejs_course_database',
+
 });
+
+await client.connect();
+
+export const db = drizzle(client, { schema });
